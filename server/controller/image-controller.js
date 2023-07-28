a9d71772117844a1a56acf22cb1f604b109e06f0
anamika1006 <86562181+anamika1006@users.noreply.github.com>
2023-07-29 00:54:04 +0530

    image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -6,7 +6,7 @@ const url= 'http://localhost:8000';
 
 export const uploadImage =(request,response)=>{
     if(!request.file){
-        return response.status(404),json({msg:"file not found"});
+        return response.status(404).json({msg:"file not found"});
     }
     const imageUrl = `${url}/file/${request.file.filename}`;
     return response.status(200).json(imageUrl);
