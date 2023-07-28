786b6b0278b46d779026932e38c732e22fe57a6b
anamika1006 <86562181+anamika1006@users.noreply.github.com>
2023-07-28 22:38:38 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,16 +1,7 @@
-import grid from 'gridfs-stream';
-import mongoose from 'mongoose';
+
 const url= 'http://localhost:8000';
 
-let gfs, gridfsBucket;
-const conn = mongoose.connection;
-conn.once('open', () => {
-    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
-        bucketName: 'fs'
-    });
-    gfs = grid(conn.db, mongoose.mongo);
-    gfs.collection('fs');
-});
+
 
 
 export const uploadImage =(request,response)=>{
@@ -20,16 +11,4 @@ export const uploadImage =(request,response)=>{
     const imageUrl = `${url}/file/${request.file.filename}`;
     return response.status(200).json(imageUrl);
 
-}
-
-export const getImage = async (request, response) => {
-    try {   
-        const file = await gfs.files.findOne({ filename: request.params.filename });
-        // const readStream = gfs.createReadStream(file.filename);
-        // readStream.pipe(response);
-        const readStream = gridfsBucket.openDownloadStream(file._id);
-        readStream.pipe(response);
-    } catch (error) {
-        response.status(500).json({ msg: error.message });
-    }
 }
